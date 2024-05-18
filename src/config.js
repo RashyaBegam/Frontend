08f2a99b1ff97928629f0f3a835112a57037e40d
velmurugan444 <mvel1620r@gmail.com>
2024-05-18 11:03:14 +0530

    updated url in config.js file

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-// export const API_BASE_URL =
-//   "https://hospital-management-system-5g9t.onrender.com/api/";
-export const API_BASE_URL = "http://localhost:3001/api/";
+export const API_BASE_URL =
+  "https://social-media-application-3kkp.onrender.com/api/";
+// export const API_BASE_URL = "http://localhost:3001/api/";
